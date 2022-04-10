deb742e5529f208b35733beac0878326db05cafa
andrianalif <alifandri15@gmail.com>
2022-04-10 18:47:31 +0700

    Create soal1.R

---
+++ soal1.R
@@ -0,0 +1,34 @@
+#no.1a
+n = 3
+p = 0.20
+dgeom(x = n, prob = p)
+#no.1b
+mean(rgeom(n = 10000, prob = p) == 3)
+
+#no.1c
+#Hasil dari perhitungan a dan b tidak terlalu jauh, bedanya kalau a itu nilainya tetap
+#sedangkan kalau b itu hasilnya random, besar kecil nilainya ditentukan oleh X
+
+#no.1d
+data.frame(x = 0:10, prob = dgeom(x = 0:10, prob = p)) %>%
+  mutate(Failures = ifelse(x == n, n, "other")) %>%
+  ggplot(aes(x = factor(x), y = prob, fill = Failures)) +
+  geom_col() +
+  geom_text(
+    aes(label = round(prob,2), y = prob + 0.01),
+    position = position_dodge(0.9),
+    size = 3,
+    vjust = 0
+  ) +
+  labs(title = "Histogram Distribusi Geometrik,Peluang X = 3 gagal Sebelum Sukses Pertama",
+       subtitle = "Geometric(.2)",
+       x = "Kegagalan sebelum sukses yang pertama (x)",
+       y = "Peluang") 
+
+#no.1e
+#Rataan
+miu = 1/p
+paste("Rataan adalah ", miu)
+#varian
+varian = (1-p)/ p^2
+paste("varian adalah ", varian)
